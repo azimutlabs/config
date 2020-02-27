ba9d793232c688a7df3503d8c33545a43c1efecc
olehan <alihan.arshabekov@gmail.com>
2020-02-27 18:09:46 +0600

    refactor(eslint): move eslint folder into separated package

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,48 @@
+# eslint-config-eslint
+Sharable ESLint configurations used in our projects.
+
+We assume that you already have `eslint` package installed.
+
++ [`legacy`](legacy.js) - default lint rules with some plugin extensions.
+  - required libs: `eslint-plugin-import`
+  - required libs for **javascript**: `babel-eslint`
+  - required libs for **typescript**: `@typescript-eslint/parser`, `@typescript-eslint/eslint-plugin`
+  - download command: `npm i -D eslint-plugin-import @typescript-eslint/parser @typescript-eslint/eslint-plugin`
++ [`prettier`](prettier.js) - prettier config for eslint.
+  - required libs: `prettier`, `eslint-config-prettier`, `eslint-plugin-prettier`
+  - download command: `npm i -D prettier eslint-config-prettier eslint-plugin-prettier`
++ [`jest`](jest.js) - jest specific rules.
+  - required libs: `eslint-plugin-jest`
+  - download command: `npm i -D eslint-plugin-jest`
++ [`react`](react.js) - react specific rules with extensions.
+  - required libs: `eslint-plugin-react`, `eslint-plugin-react-hooks`
+  - download command: `npm i -D eslint-plugin-react eslint-plugin-react-hooks`
++ [`a11y`](a11y.js) - jsx accessibility.
+  - required libs: `eslint-plugin-jsx-a11y`
+  - download command: `npm i -D eslint-plugin-jsx-a11y`
+
+#### Recommended usage
+Extending ALL configs via `package.json`:
+```json
+{
+  "eslintConfig": {
+    "extends": [
+      "@azimutlabs"
+    ]
+  }
+}
+```
+Extending configs separately:
+```json
+{
+  "eslintConfig": {
+    "extends": [
+      "@azimutlabs/eslint-config/legacy",
+      "@azimutlabs/eslint-config/prettier",
+      "@azimutlabs/eslint-config/react"
+    ]
+  }
+}
+```
+
+## [`LICENSE`](LICENSE)
---
+++ packages/eslint-config/react.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2019 Azimut Labs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ */
+
+'use strict';
+
+module.exports = {
+  extends: [
+    'plugin:react/recommended'
+  ],
+  plugins: [
+    'react-hooks'
+  ],
+  env: {
+    browser: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: [
+        '**/next-env.d.ts'
+      ],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      }
+    },
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/forbid-foreign-prop-types': [
+      'warn',
+      {
+        allowInPropTypes: true
+      }
+    ],
+    'react/jsx-no-comment-textnodes': 'warn',
+    'react/jsx-no-duplicate-props': 'warn',
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-no-undef': 'error',
+    'react/jsx-pascal-case': [
+      'warn',
+      {
+        allowAllCaps: true,
+        ignore: []
+      }
+    ],
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/no-danger-with-children': 'warn',
+    'react/no-direct-mutation-state': 'warn',
+    'react/no-is-mounted': 'warn',
+    'react/no-typos': 'error',
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
+    'react/style-prop-object': 'warn',
+    'jsx-quotes': [
+      'error',
+      'prefer-double'
+    ]
+  }
+};

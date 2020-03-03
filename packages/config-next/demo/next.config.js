82f605c6e4876818c77b603f356a697805265d5d
olehan <alihan.arshabekov@gmail.com>
2020-03-03 17:25:12 +0600

    feat(config-next): add withStylelint

--- packages/config-next/demo/next.config.js
+++ packages/config-next/demo/next.config.js
@@ -1,7 +1,9 @@
+const withStylelint = require('../withStylelint');
 const withEmotion = require('../withEmotion');
 const withEslint = require('../withEslint');
+const withLodash = require('../withLodash');
 const withSass = require('../withSass');
 const withEnv = require('../withEnv');
 const { compose } = require('../utils');
 
-module.exports = compose(withEmotion, withEslint, withSass, withEnv)();
+module.exports = compose(withStylelint, withLodash, withEmotion, withEslint, withSass, withEnv)();

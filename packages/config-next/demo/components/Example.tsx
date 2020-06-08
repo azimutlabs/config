3e7fd409f07832e4fc77413c52ac73c6a0329731
olehan <alihan.arshabekov@gmail.com>
2020-06-08 21:52:48 +0600

    refactor!: remove config-next for further refactoring

--- packages/config-next/demo/components/Example.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, PropsWithChildren } from 'react';
-
-import styles from './Example.module.scss';
-
-type ExampleProps = PropsWithChildren<{}>;
-
-export const Example: FC<ExampleProps> = ({ children }: ExampleProps) => (
-  <div className={styles.example}>{children}</div>
-);
--- packages/config-next/demo/next.config.js
+++
@@ -1,9 +0,0 @@
-const withStylelint = require('../withStylelint');
-const withEmotion = require('../withEmotion');
-const withEslint = require('../withEslint');
-const withLodash = require('../withLodash');
-const withSass = require('../withSass');
-const withEnv = require('../withEnv');
-const { compose } = require('../utils');
-
-module.exports = compose(withStylelint, withLodash, withEmotion, withEslint, withSass, withEnv)();
